4aa0b782f55a10c02c760c3066c8178722a142c8
Vinicius Araújo <162530634+varaujod@users.noreply.github.com>
2024-07-25 13:29:33 -0300

    Add files via upload

---
+++ Projeto2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+Receita de bolo
+Autor: Vinicius Araújo
+Data de criação: 17/07/2024
+Versão 1.0
+ */
+
+int main() {
+	printf("Bolo de Cenoura \n \n");
+	printf("Ingredientes: \n");
+	printf("1/2 xicara (cha) de oleo \n");
+	printf("3 cenouras medias raladas \n");
+	printf("4 ovos \n");
+	printf("2 e 1/2 xicaras (cha) de farinha de trigo \n");
+	printf("2 xícaras (chá) de acucar \n");
+	printf("1 colher (sopa) de fermento em po \n \n");
+	printf("Modo de Preparo: \n");
+	printf("Em um liquidificador, adicione a cenoura, os ovos e o oleo, depois misture. \n");
+	printf("Acrescente o acucar e bata novamente por 5 minutos. \n");
+	printf("Em uma tigela ou na batedeira, adicione a farinha de trigo e depois misture novamente. \n");
+	printf("Acrescente o fermento e misture lentamente com uma colher. \n");
+	printf("Asse em um forno preaquecido a 180C por aproximadamente 40 minutos. \n \n");
+	printf("E pronto, agora voce pode deliciar o seu bolo! ");
+}
